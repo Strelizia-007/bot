d568ebf5ce24608b60f763445f16ae055072a14a
Vazhasnm <109035957+Vazhasnm@users.noreply.github.com>
2023-04-04 19:00:42 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -7,7 +7,7 @@ try:
     APP_ID = 6620972
     API_HASH = "3f6835286b03e000ab6d71b37cc35b92"
     BOT_TOKEN = "5554850508:AAEr9ZKxdRb5C3zRDt-pmcEBzn6rq7ZmtmI"
-    DEV = 1664850827
+    DEV = 5674020159
     OWNER = 1745047302
     ffmpegcode = ["-preset faster -c:v libx265 -s 854x480 -x265-params 'bframes=8:psy-rd=1:ref=3:q-mode=3:aq-strength=0.8:deblock=1,1' -metadata 'title=Encoded By TGVid-Comp (https://github.com/Zylern/TGVid-Comp)' -pix_fmt yuv420p -crf 30 -c:a libopus -b:a 32k -c:s copy -map 0 -ac 2 -ab 32k -vbr 2 -level 3.1 -threads 1"]
     THUMBNAIL = "https://telegra.ph/file/f63d27e8d9fe0792d9017.jpg"
